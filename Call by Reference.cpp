6e7608b5c1b2abf4294dd3f3cd30f29652482123
alok-003 <alokchawat@gmail.com>
2024-09-01 12:47:11 +0530

    Add files via upload

---
+++ Call by Reference.cpp
@@ -0,0 +1,25 @@
+//Name:Alok Chawat
+//PRN:23070123016
+//Aim:Implemention of Pointer Operations to Call by Reference
+
+#include<iostream>
+using namespace std;
+void swap(int *x, int *y)
+{
+    int swap;
+    swap=*x;
+    *x=*y;
+    *y=swap;
+}
+int main(){
+    int x=500,y=100;
+    swap(x,y);
+    cout<<"Value of x is:"<<x<<endl;
+    cout<<"Value of y is:"<<y<<endl;
+    return 0;
+}
+
+
+/*Output:
+Value of x is:100
+Value of y is:500*/
---
+++ Call by Value.cpp
@@ -0,0 +1,26 @@
+//Name:Alok Chawat
+//PRN:23070123016
+//Aim:Implemention of Pointer Operations to Call by Value
+
+#include<iostream>
+using namespace std;
+void swap(int x, int y)
+{
+    int swap;
+    swap=x;
+    x=y;
+    y=swap;
+}
+int main(){
+    int a=5,b=2;
+    swap(a,b);
+    cout<<"Value of a is:"<<a<<endl;
+    cout<<"Value of b is:"<<b<<endl;
+    return 0;
+}
+
+
+/*Output:
+Value of a is:5
+Value of b is:2
+*/
